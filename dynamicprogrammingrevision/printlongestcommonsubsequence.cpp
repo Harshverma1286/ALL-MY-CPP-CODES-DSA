b05f74b213f948e149a362f87d98a3e3eae98d88
Harsh Verma <hv0626266@gmail.com>
2025-11-17 15:46:28 +0530

    feat: implement longest common subsequence algorithm and add executable

--- dynamicprogrammingrevision/printlongestcommonsubsequence.cpp
+++ dynamicprogrammingrevision/printlongestcommonsubsequence.cpp
@@ -1,16 +1,9 @@
 #include<iostream>
+#include<vector>
 #include<string>
 #include<limits.h>
+#include<algorithm>
 using namespace std;
-
-// string lcs(string a,string b,string ans,int i,int j){
-//     if(i==0 || j==0) return " ";
-
-//     if(a[i-1]==b[j-1]){
-//         return ans+lcs(a,b,ans,i-1,j-1);
-//     }
-//     else return 
-// }
 
 int main(){
     string a = "acbcf";
@@ -19,8 +12,39 @@ int main(){
 
     string ans = "";
 
+    vector<vector<int>> dp(a.length()+1,vector<int>(b.length()+1,0));
+
+
+    for(int i=1;i<a.length()+1;i++){
+        for(int j=1;j<b.length()+1;j++){
+            if(a[i-1]==b[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+
     int i = a.length();
     int j = b.length();
 
-    //cout<<lcs(a,b,ans,i,j);
+    while(i>0 && j>0){
+        if(a[i-1]==b[j-1]){
+            ans+=a[i-1];
+            i=i-1;
+            j=j-1;
+        }
+        else{
+            if(dp[i][j-1]>dp[i-1][j]){
+                j = j-1;
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
+                i=i-1;
+            }
+        }
+    }
+
+
+    reverse(ans.begin(),ans.end());
+
+    cout<<ans;
 }
